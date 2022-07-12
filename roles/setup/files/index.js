98c6a8e4b6850843e05002e5528b1cd4054822b9
mohab4717 <107080258+mohab4717@users.noreply.github.com>
2022-07-12 16:19:24 +0200

    Update index.js

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -1,5 +1,5 @@
 var http = require("http");
-var server = createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
 res.writeHead(200);
 res.end("Hello world!");
 });
